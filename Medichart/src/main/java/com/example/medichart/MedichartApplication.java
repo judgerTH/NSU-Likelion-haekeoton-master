bbf4952ab723e24dbedf90920d4b91a92347b3c2
judgerTH <disney1026dlxogus@gmail.com>
2024-07-14 00:02:50 +0900

    first commit

---
+++ Medichart/src/main/java/com/example/medichart/MedichartApplication.java
@@ -0,0 +1,13 @@
+package com.example.medichart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedichartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedichartApplication.class, args);
+	}
+
+}
---
+++ Medichart/src/test/java/com/example/medichart/MedichartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medichart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedichartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
